3770ba49334845ff041c1225a1c40a0eb07c1e73
mosev1474 <zaffedontknowhimself@gmail.com>
2026-02-22 19:00:31 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function checkPass() {
-  let pass = document.getElementById("pass").value;
-
-  if (pass === "1234") {
-    window.location.href = "love.html";
-  } else {
-    alert("الباسورد غلط يا نجم 😏");
-  }
-}
